f29f29d63310c8731fe78cdd1188dce67a176ec3
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2022-07-09 21:11:49 +0700

    check word and try again

--- component/learn/fillTheBlank.tsx
+++ component/learn/fillTheBlank.tsx
@@ -3,15 +3,21 @@ import { useState } from 'react';
 import { useRef } from 'react';
 import { FiDelete } from "react-icons/fi";
 export interface IFillBlankProps {
-  word: string,
-  meaning: string
+  word: {
+    word: string,
+    meaning: string,
+    pronun: string,
+    sentence: string,
+  }
 }
 
 export default function FillBlank (props: IFillBlankProps) {
   // console.log("props", props);
-  const inputRef = props.word.split('').map((val) =>useRef(null));
+  const inputRef = props.word.word.split('').map((val) =>useRef(null));
   const [currentInputIndex, setCurrentInputIndex] = useState(0);
   const [isComplete, setIsComplete] = useState(false);
+  const [isFinishCheck, setIsFinishCheck] = useState(false);
+  const [isTrueCheck, setIsTrueCheck] = useState(false);
   const deleteChar = () => {
     if(currentInputIndex === 0){
       (inputRef[currentInputIndex].current as unknown as HTMLInputElement).value = '';
@@ -22,7 +28,7 @@ export default function FillBlank (props: IFillBlankProps) {
       (inputRef[currentInputIndex].current as unknown as HTMLInputElement).value = '';
       (inputRef[currentInputIndex -1].current as unknown as HTMLInputElement).focus();
       setCurrentInputIndex(currentInputIndex-1);
-      if(currentInputIndex === props.word.length - 1){
+      if(currentInputIndex === props.word.word.length - 1){
         setIsComplete(false);
       }
     }
@@ -37,14 +43,16 @@ export default function FillBlank (props: IFillBlankProps) {
     <div className='text-center'
       onKeyDown={keyDownHandler}
     >
-      <div className='w-[60%] mx-auto'>
+      <div className='w-[60%] mx-auto uppercase mt-2 border-[1px] 
+        mb-[10px] bg-[#797ec7] text-[#fff] font-[600] py-[4px] rounded-[8px]'>
         Fill in the blank
       </div>
-      <label className='word uppercase text-[#404bdf] font-[700] text-[28px]'>
-        {props.meaning}
+      <label className='word uppercase text-[#404bdf] font-[700] text-[28px] mt-[20px]'>
+        {props.word.meaning}
       </label>
+      {/* Word Input */}
       <div className="word-container flex justify-center relative">
-        <div className="border-[3px] border-[#7c83df] mt-[14px] pb-[24px] pt-[12px] flex hover:opacity-70 justify-self-center relative"
+        <div className="border-[3px] border-[#7c83df] mt-[14px] pb-[22px] pt-[16  px] flex hover:opacity-70 justify-self-center relative"
           onClick={(event: React.MouseEvent<HTMLDivElement>) => {
             console.log("event.target", event.target);
             const attributes: any = (event.target as HTMLElement).attributes;
@@ -62,7 +70,7 @@ export default function FillBlank (props: IFillBlankProps) {
         >
           <div className='input-container flex ml-[24px] mr-[18px]'>
             {
-              props.word.split('').map((val, index) => {
+              props.word.word.split('').map((val, index) => {
                 return (
                   <div className='input-check' key={index}>
                     <input 
@@ -100,27 +108,63 @@ export default function FillBlank (props: IFillBlankProps) {
         </div>
         
       </div>
-      
-      <button 
-        className={
-          isComplete 
-            ?"check-button bg-[#7c83df] text-[#fff] px-[12px] py-1 mt-[82px] hover:opacity-70"
-            :"check-button bg-[#85858a] text-[#fff] px-[12px] py-1 mt-[82px] hover:opacity-70"
-        }
-        onClick={() => {
-          if(isComplete){
-            if(inputRef.every((val, index) => ((val.current as unknown as HTMLInputElement).value.toLowerCase() === props.word[index].toLowerCase()))){
-              alert("Đúng rồi")
-            }
-            else {
-              alert("Sai rồi")
-            }
+      {/* Result */}
+      <div className="check mt-[20px] text-left h-[14s8px] mx-3">
+        {
+          !(isFinishCheck) ? null :
+          <div className={
+            isTrueCheck 
+            ? "word-container bg-[#22a636] py-[10px] px-[20px] text-[#fff] rounded-[12px]" 
+            : "word-container bg-[#d65129] py-[10px] px-[20px] text-[#fff] rounded-[12px]"
+          }>
+            <label className='word'>{props.word.word.toLowerCase()}</label>
+            <p className="pronunc">{props.word.pronun}</p>
+            <p className='meaning'>{props.word.meaning}</p>
+            <p className="sentence">{props.word.sentence}</p>
 
+          </div> 
+        }
+      </div>
+      {
+        !(isComplete&&isFinishCheck) ?
+        <button 
+          className={
+            isComplete 
+              ?"check-button bg-[#7c83df] text-[#fff] px-[12px] py-1 mt-[24px] hover:opacity-70 rounded-[4px] w-[60%]"
+              :"check-button bg-[#85858a] text-[#fff] px-[12px] py-1 mt-[24px] hover:opacity-70 rounded-[4px] w-[60%]" 
           }
-        }}
-      >
-        Check
-      </button>
+          onClick={() => {
+            if(isComplete){
+              setIsFinishCheck(true);
+              if(inputRef.every((val, index) => ((val.current as unknown as HTMLInputElement).value.toLowerCase() === props.word.word[index].toLowerCase()))){
+                setIsTrueCheck(true);
+              }
+              else {
+                setIsTrueCheck(false);
+                
+              }
+
+            }
+          }}
+        >
+          Check
+        </button>
+        : 
+        <button 
+          className="next check-button bg-[#7c83df] text-[#fff] px-[12px] py-1 mt-[24px] hover:opacity-70 rounded-[4px] w-[60%]"
+          onClick={() => {
+            setIsComplete(false);
+            setIsFinishCheck(false);
+            setIsTrueCheck(false);
+            inputRef.forEach((ref) => {
+              (ref.current as unknown as HTMLInputElement).value = '';
+            })
+          }}
+        >
+          Try again
+        </button>
+      }
+      
     </div>
       
   );
--- component/learn/question.tsx
+++ component/learn/question.tsx
@@ -8,15 +8,19 @@ export interface IQuestionProps {
 export default function Question (props: IQuestionProps) {
 
     return (
-      <div className='text-center '>
+      <div className='text-center h-[80vh]'>
         <h2 className='text-[#7c83df] font-[700] text-[24px]'>ÔN TẬP TỪ VỰNG</h2>
-        <div className='w-[60%] mx-auto mt-[20px]'>
+        {/* <div className='w-[60%] mx-auto mt-[20px]'>
             <ProgressBar completed={60} />
-        </div>
-        <div className="question-container mt-[12px]">
+        </div> */}
+        <div className="question-container mt-[12px] h-[80%]">
           <FillBlank 
-            word="king"
-            meaning='vua'
+            word={{
+              word: "AMBIGIOUS",
+              meaning: "Mơ hồ",
+              pronun: "/æmˈbɪɡ.ju.əs/",
+              sentence: "His reply to my question was somewhat ambiguous."
+            }}
           />
         </div>
       </div>
